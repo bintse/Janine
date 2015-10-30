bcd6c22353390f134039f05a65a46b195031823d
bintse <bintse@qq.com>
2015-10-30 17:51:04 +0800

    commit

--- src/main/java/com/janine/control/LoginControl.java
+++ src/main/java/com/janine/control/LoginControl.java
@@ -1,38 +1,40 @@
 package com.janine.control;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.janine.entity.User;
-
 @Controller
-@RequestMapping("/login")
+//@RequestMapping("/login")
 public class LoginControl {
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("------>login");
-		
-        User user = new User();  
-        user.setUsername("zhangsan");  
-        user.setPassword("1234");  
-        List<User> users = new ArrayList<User>();  
-        users.add(user);  
-        return new ModelAndView("user", "users", users);  
-        
-	}
+    @RequestMapping(value = "/greeting/{user}", method = RequestMethod.GET)
+    public String greeting(@PathVariable String user, Model model) {
+        List<String> userList = Arrays.asList(user.split("-"));
+        //userList is the variable name, used in ftl file.
+        model.addAttribute("userList", userList);
+        return "welcome";
+    }
 
-	@RequestMapping(value = "/login1", method = RequestMethod.POST)
-	public String doLogin() {
-		System.out.println("------>doLogin");
-		return "login";
-	}
+    @RequestMapping(value = "/greeting", method = RequestMethod.POST)
+    public ModelAndView greeting(@RequestParam("user") String user) {
+        List<String> userList = Arrays.asList(user.split("-"));
+        ModelAndView result = new ModelAndView("welcome");
+        //userList is the variable name, used in ftl file.
+        result.addObject("userList", userList);
+        return result;
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+    	 System.out.println("===================>");
+        return "login";
+    }
 }
