7e64bb4af18a96153778b01c817d81270fca7875
bintse <bintse@qq.com>
2015-11-11 14:46:18 +0800

    add encrypt

---
+++ src/main/java/com/janine/util/SnovaPasswordService.java
@@ -0,0 +1,25 @@
+package com.janine.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authc.credential.PasswordService;
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.crypto.hash.Sha512Hash;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SnovaPasswordService implements PasswordService {
+    private static final String SNOVA_SALT = "janine";// password salt
+
+    @Override
+    public String encryptPassword(Object plaintextPassword) throws IllegalArgumentException {
+        String base64Sha512HashPwd = new Sha512Hash(plaintextPassword, SNOVA_SALT).toBase64();
+        return new Md5Hash(base64Sha512HashPwd, SNOVA_SALT).toHex();
+    }
+
+    @Override
+    public boolean passwordsMatch(Object submittedPlaintext, String encrypted) {
+        String pwd = (String) submittedPlaintext;
+        return StringUtils.equals(encryptPassword(pwd), encrypted);
+    }
+
+}
